271b9c59d48db61a089a06195bb31b6e34bed195
Igor Pavlov <igortvk@ya.ru>
2020-12-06 17:22:52 +0300

    Added test

---
+++ src/test/java/JenkinsExampleTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+public class JenkinsExampleTest {
+    @Test
+    void helloJenkins() {
+        String str1 = "jenkins";
+        String str2 = "JENKINS";
+
+        System.out.println(str1.equalsIgnoreCase(str2));
+
+
+
+    }
+
+}
+
+
