e3b1755b06f69d7384646c1a489b89a1027dcdd4
Igor Pavlov <igortvk@ya.ru>
2020-12-17 22:23:36 +0300

    added telegram bot

--- src/test/java/helpers/WebConfig.java
+++ src/test/java/helpers/WebConfig.java
@@ -6,8 +6,8 @@ import org.aeonbits.owner.Config;
     @Config.LoadPolicy(Config.LoadType.MERGE)
     @Config.Sources({
             "system:properties",
-            //"classpath:${environment}.properties",
-            "file:~/project.properties"
+            "classpath:${environment}.properties",
+            //"file:~/project.properties"
     })
 
     public interface WebConfig extends Config {
